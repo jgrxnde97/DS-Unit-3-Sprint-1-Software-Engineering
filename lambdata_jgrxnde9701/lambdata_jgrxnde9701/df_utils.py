e5878cb8fb50000da6c90836c05da092303f84f3
jgrxnde97 <55990565+jgrxnde97@users.noreply.github.com>
2019-12-06 04:34:18 -0800

    Add files via upload

---
+++ lambdata_jgrxnde9701/lambdata_jgrxnde9701/df_utils.py
@@ -0,0 +1,12 @@
+"""
+lambdata_jgrxnde9701 - utility functions for working with DataFrames
+"""
+
+# codes
+import pandas as pd
+import numpy as np
+
+# dataframes
+df_null = pd.DataFrame([2, 2, 3, 5, np.NaN, 7, 8, 9, 9, np.NaN])
+df_ran = pd.DataFrame(np.random.randn(200, 5))
+df_test = pd.DataFrame([1, 2, 4, 4, 6, 7, 8, 0])
